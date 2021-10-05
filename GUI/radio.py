c813278502a3981db6e6b82ed44caac6f92556da
elugo13 <lugoerik00@gmail.com>
2021-10-04 23:16:46 -0500

    Update window titles

--- GUI/radio.py
+++ GUI/radio.py
@@ -9,7 +9,7 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        self.setWindowTitle("Buttons window")
+        self.setWindowTitle("Radio buttons")
         self.resize(1280, 720)
         layout = QVBoxLayout()
 
